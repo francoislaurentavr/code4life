b3c09f1e757ac7c585941a7387712ad31169b1fc
flaurentavr <laurent.francois.dev@gmail.com>
2017-05-13 01:14:08 +0200

    ligue_bronze

--- code4life.js
+++ code4life.js
@@ -1,7 +1,3 @@
-/**
- * Bring data on patient samples from the diagnosis machine to the laboratory with enough molecules to produce medicine!
- **/
-
 var read = ()=>readline().split(' '),
     pos=a=>+a>0?+a:0,
     abcde = (a,b,c,d,e)=>({
@@ -14,22 +10,23 @@ var read = ()=>readline().split(' '),
             if(this.c>0) r='c';
             if(this.b>0) r='b';
             if(this.a>0) r='a';
-            return r;            
+            return r;
         }
     }),
     difabcde=(a,b)=>abcde(a.a-b.a,a.b-b.b,a.c-b.c,a.d-b.d,a.e-b.e),
+    addabcde=(a,b)=>abcde(a.a+b.a,a.b+b.b,a.c+b.c,a.d+b.d,a.e+b.e),
     minabcde=(a,b)=>abcde(Math.min(a.a,b.a),Math.min(a.b,b.b),Math.min(a.c,b.c),Math.min(a.d,b.d),Math.min(a.e,b.e)),
     readArr=(func)=>[...Array(+readline())].map(a=>func()),
     robot = ()=>{
         var t,e,s,sa,sb,sc,sd,se,ea,eb,ec,ed,ee;
         [t,e,s,sa,sb,sc,sd,se,ea,eb,ec,ed,ee] = read();
         return {
-                target:t,
-                eta: +e,
-                score:+s,
-                storage:abcde(sa,sb,sc,sd,se),
-                expertise:abcde(ea,eb,ec,ed,ee)
-            };
+            target:t,
+            eta: +e,
+            score:+s,
+            storage:abcde(sa,sb,sc,sd,se),
+            expertise:abcde(ea,eb,ec,ed,ee)
+        };
     },
     rabcde=()=>abcde(...read()),
     sample= ()=>{
@@ -45,20 +42,36 @@ var read = ()=>readline().split(' '),
         };
     },
     modules={diag:'DIAGNOSIS',mol:'MOLECULES',lab:'LABORATORY',samp:'SAMPLES'},
-    readGame = ()=>{ 
-        return {            
-          me:robot(),
-          other:robot(),
-          availlables:rabcde(),
-          samples:readArr(sample),
-          filtSample:function(prop){return this.samples.filter(s=>s.prop===prop)},
-          carSamples:function(){return this.filtSample(0);},
-          availSamples:function(){return this.filtSample(-1)}      
+    readGame = ()=>{
+        return {
+            me:robot(),
+            other:robot(),
+            availlables:rabcde(),
+            samples:readArr(sample),
+            filtSample:function(prop){return this.samples.filter(s=>s.prop===prop)},
+            carSamples:function(){return this.filtSample(0);},
+            availSamples:function(){return this.filtSample(-1)}
         };
     },
     goto=(mod)=>print('GOTO',mod),
     connect=(o)=>print('CONNECT',(''+o).toUpperCase()),
-    debug=(o)=>printErr(JSON.stringify(o));
+    debug=(o)=>printErr(JSON.stringify(o)),
+    findTake=(samp,storage,expertise,avail)=>{
+        var need = difabcde(samp.cost,addabcde(storage,expertise)),
+            totneed = need.tot();
+        if(totneed >0){
+            var needAndAvail = minabcde(need,avail),
+                toTake = needAndAvail.firstPos(),
+                needAvailTot = needAndAvail.tot();
+            if(needAvailTot < totneed || needAvailTot + storage.tot() > 10){
+                //tout n'est pas dispo pour ce sample, check others
+                return false;
+            }
+            return toTake;
+        }else{
+            return true;
+        }
+    };
 
 //  Init
 var projects = readArr(rabcde);
@@ -67,80 +80,30 @@ var steps=["takeSample","diag","takeMol","lab"],
     step = steps[0],
     nextStep=()=>{step = steps[(steps.indexOf(step)+1)%steps.length]};
 
-// var steps = [
-//     (me,samples)=>'GOTO DIAGNOSIS',
-//     ()=>''
-// ]
-
-// game loop
 while (true) {
-    // for (var i = 0; i < 2; i++) {
-    //     var inputs = readline().split(' ');
-    //     var target = inputs[0];
-    //     var eta = parseInt(inputs[1]);
-    //     var score = parseInt(inputs[2]);
-    //     var storageA = parseInt(inputs[3]);
-    //     var storageB = parseInt(inputs[4]);
-    //     var storageC = parseInt(inputs[5]);
-    //     var storageD = parseInt(inputs[6]);
-    //     var storageE = parseInt(inputs[7]);
-    //     var expertiseA = parseInt(inputs[8]);
-    //     var expertiseB = parseInt(inputs[9]);
-    //     var expertiseC = parseInt(inputs[10]);
-    //     var expertiseD = parseInt(inputs[11]);
-    //     var expertiseE = parseInt(inputs[12]);
-    // }
-    // var inputs = readline().split(' ');
-    // var availableA = parseInt(inputs[0]);
-    // var availableB = parseInt(inputs[1]);
-    // var availableC = parseInt(inputs[2]);
-    // var availableD = parseInt(inputs[3]);
-    // var availableE = parseInt(inputs[4]);
-    // var sampleCount = parseInt(readline());
-    // for (var i = 0; i < sampleCount; i++) {
-    //     var inputs = readline().split(' ');
-    //     var sampleId = parseInt(inputs[0]);
-    //     var carriedBy = parseInt(inputs[1]);
-    //     var rank = parseInt(inputs[2]);
-    //     var expertiseGain = inputs[3];
-    //     var health = parseInt(inputs[4]);
-    //     var costA = parseInt(inputs[5]);
-    //     var costB = parseInt(inputs[6]);
-    //     var costC = parseInt(inputs[7]);
-    //     var costD = parseInt(inputs[8]);
-    //     var costE = parseInt(inputs[9]);
-    // }
-    // Write an action using print()
-    // To debug: printErr('Debug messages...');
-    
     var game = readGame();
-    //printErr(JSON.stringify(game));
-    var carSample = game.carSamples(),
+    var carSample = game.carSamples().sort((a,b)=>a.rank-b.rank),
         target = game.me.target;
-      
-    debug({step:step})
+
+    debug({step:step});
+    debug({me:game.me});
+    debug({carSample:carSample});
     switch(step){
-        case steps[0]:        
-            
-            if(target != modules.samp){
+        case steps[0]:
+
+            if(target != modules.samp || game.me.eta !=0){
                 goto(modules.samp);
             }else{
-                // var samps = game.availSamples().sort((a,b)=>b.rank-a.rank);
-                // if(samps.length >0 && carSample.length < 1){             
-                //     connect(samps[carSample.length>1 ? samps.length-1:0].id);
-                // }else{
-                //     
-                // }
-                if(carSample.length == 0){
-                    connect(2);
+                if(carSample.length < 3){
+                    connect(2);//(carSample.length >0 && carSample[0].rank==2) ? 1:2);
                 }else{
                     nextStep();
                     goto(modules.diag);
                 }
-            }            
+            }
             break;
         case steps[1]:
-            if(target  != modules.diag){
+            if(target  != modules.diag || game.me.eta !=0){
                 goto(modules.diag);
             }else{
                 var toDiag = carSample.filter(a=>a.cost.tot()==0);
@@ -153,45 +116,71 @@ while (true) {
             }
             break;
         case steps[2]:
-            // var ok=[];
-            // for(var i=0;i< carSample.length;i++){
-            if(target != modules.mol){
-                goto(modules.mol);   
+            if(target != modules.mol || game.me.eta !=0){
+                goto(modules.mol);
             }else{
-                var need = difabcde(carSample[0].cost,game.me.storage),
-                    totneed = need.tot();
-                    
-                if(totneed >0){                    
-                    var needAndAvail = minabcde(need,game.availlables),
-                        toTake = needAndAvail.firstPos();
-                    debug(needAndAvail);
-                    debug(toTake);
-                    if(toTake !== false){
-                        connect(toTake);                    
-                    }else{                    
-                        printErr("nothing to take :(");
-                        goto(modules.diag);
-                    }                                
-                }else{
-                    nextStep();
-                    goto(modules.lab);
-                } 
+                var ok = false, oneFinish=false;
+                for(var i = 0; i < carSample.length;i++){
+                    var toTake = findTake(carSample[i],game.me.storage,game.me.expertise, game.availlables);
+                    if(toTake === true){
+                        oneFinish = true;
+                    }else if(toTake !== false){
+                        connect(toTake);
+                        ok = true;
+                        break;
+                    }
+                }
+                if(!ok){
+                    printErr("nothing to take :(");
+                    if(oneFinish){
+                        nextStep();
+                        goto(modules.lab);
+                    }else if(carSample.length < 3) {
+                        step = steps[0];
+                        goto(modules.samp);
+                    }else {
+                        if(game.other.target == modules.lab){
+                            print('WAIT');
+                        }else {
+                            step = 'removeSample';
+                            goto(modules.diag);
+                        }
+                    }
+                }
             }
             break;
         case steps[3]:
-            if(target != modules.lab){
+            if(target != modules.lab || game.me.eta !=0){
                 goto(modules.lab);
             }else{
                 if(carSample.length>0){
-                    connect(carSample[0].id);
+                    var ok2=false;
+                    for(var j = 0; j < carSample.length;j++) {
+                        if (difabcde(carSample[j].cost, addabcde(game.me.storage,game.me.expertise)).tot() == 0) {
+                            connect(carSample[j].id);
+                            ok2 = true;
+                            break;
+                        }
+                    }
+                    if(!ok2){
+                        step = 'takeMol';
+                        goto(modules.mol);
+                    }
                 }
                 else
                 {
                     nextStep();
                     goto(modules.samp);
-                }                    
+                }
+            }
+            break;
+        case 'removeSample':
+            if(target  != modules.diag || game.me.eta !=0){
+                goto(modules.diag);
+            }else {
+                connect(carSample[0].id);
+                step = 'takeSample';
             }
             break;
     }
-        
 }
